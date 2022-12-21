0dc485553dad84c13e64a9f36c0269e6c2e04234
junny <webman7@naver.com>
2022-12-21 13:16:17 +0900

    :art: 소재 지면 파일 추가

---
+++ src/main/java/com/adplatform/restApi/domain/creative/domain/PlacementCreative.java
@@ -0,0 +1,28 @@
+package com.adplatform.restApi.domain.creative.domain;
+
+import com.adplatform.restApi.domain.placement.domain.Placement;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "placement_creative_info")
+public class PlacementCreative {
+    @EmbeddedId
+    private final PlacementCreativeId id = new PlacementCreativeId();
+
+    @MapsId("placementId")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "placement_info_id")
+    private Placement placement;
+
+    @MapsId("creativeId")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "creative_info_id")
+    private Creative creative;
+}
