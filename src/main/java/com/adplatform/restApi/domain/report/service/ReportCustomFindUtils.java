efd55b842efbffd588aaf11ae4ca03ecbd5a2459
junny <webman7@naver.com>
2023-01-12 11:16:50 +0900

    :zap: 리포트 네이게이션 수정

--- src/main/java/com/adplatform/restApi/domain/report/service/ReportCustomFindUtils.java
+++ src/main/java/com/adplatform/restApi/domain/report/service/ReportCustomFindUtils.java
@@ -3,9 +3,10 @@ package com.adplatform.restApi.domain.report.service;
 import com.adplatform.restApi.domain.campaign.exception.CampaignNotFoundException;
 import com.adplatform.restApi.domain.report.dao.custom.ReportCustomRepository;
 import com.adplatform.restApi.domain.report.domain.ReportCustom;
+import com.adplatform.restApi.domain.report.exception.ReportCustomNotFoundException;
 
 public class ReportCustomFindUtils {
     public static ReportCustom findByIdOrElseThrow(Integer id, ReportCustomRepository repository) {
-        return repository.findById(id).orElseThrow(CampaignNotFoundException::new);
+        return repository.findById(id).orElseThrow(ReportCustomNotFoundException::new);
     }
 }
