d55feb3691a93ee8529c46143d7638eb11091449
Park hyunjoon <webman7@naver.com>
2023-04-12 10:14:26 +0900

    :art: 파일 다운로드 api 수정 (확장자 추가)

--- src/main/java/com/adplatform/restApi/infra/file/api/FileApi.java
+++ src/main/java/com/adplatform/restApi/infra/file/api/FileApi.java
@@ -33,21 +33,56 @@ public class FileApi {
                 .body(this.fileService.findByName(filename));
     }
 
+//    @GetMapping(path = "/downloadFile")
+//    public ResponseEntity<ByteArrayResource> downloadFile(@RequestParam String fileUrl, @RequestParam String fileName) {
+//
+//        try{
+//            byte[] data = awsFileService.download(fileUrl);
+//            ByteArrayResource resource = new ByteArrayResource(data);
+//            return ResponseEntity
+//                    .ok()
+//                    .contentLength(data.length)
+//                    .header("Content-type", "application/pdf")
+//                    .header("Content-disposition", "inline; filename=\"" + URLEncoder.encode(fileUrl, "UTF-8").replaceAll("\\+", "%20") + "\"")
+//                    .body(resource);
+//        }catch (IOException ex){
+//            return ResponseEntity.badRequest().contentLength(0).body(null);
+//        }
+//    }
+
     @GetMapping(path = "/downloadFile")
-    public ResponseEntity<ByteArrayResource> downloadFile(@RequestParam String fileUrl, @RequestParam String fileName) {
+    public ResponseEntity<byte[]> downloadFile(@RequestParam String fileUrl, @RequestParam String fileName) { // 객체 다운  fileUrl : 폴더명/파일네임.파일확장자
 
         try{
-            byte[] data = awsFileService.download(fileUrl);
-            ByteArrayResource resource = new ByteArrayResource(data);
-            return ResponseEntity
-                    .ok()
-                    .contentLength(data.length)
-                    .header("Content-type", "application/pdf")
-                    .header("Content-disposition", "inline; filename=\"" + URLEncoder.encode(fileUrl, "UTF-8").replaceAll("\\+", "%20") + "\"")
-                    .body(resource);
+            byte[] bytes = awsFileService.download(fileUrl);
+            HttpHeaders httpHeaders = new HttpHeaders();
+            httpHeaders.setContentType(contentType(fileUrl));
+            httpHeaders.setContentLength(bytes.length);
+            String[] arr = fileUrl.split("/");
+            String type = arr[arr.length - 1];
+            String fileName1 = URLEncoder.encode(type, "UTF-8").replaceAll("\\+", "%20");
+            httpHeaders.setContentDispositionFormData("attachment", fileName1); // 파일이름 지정
+
+            return new ResponseEntity<>(bytes, httpHeaders, HttpStatus.OK);
         }catch (IOException ex){
             return ResponseEntity.badRequest().contentLength(0).body(null);
         }
     }
 
+    private MediaType contentType(String keyName) {
+        String[] arr = keyName.split("\\.");
+        String type = arr[arr.length - 1];
+        switch (type) {
+            case "txt":
+                return MediaType.TEXT_PLAIN;
+            case "png":
+                return MediaType.IMAGE_PNG;
+            case "jpg":
+                return MediaType.IMAGE_JPEG;
+            case "pdf":
+                return MediaType.APPLICATION_PDF;
+            default:
+                return MediaType.APPLICATION_OCTET_STREAM;
+        }
+    }
 }
