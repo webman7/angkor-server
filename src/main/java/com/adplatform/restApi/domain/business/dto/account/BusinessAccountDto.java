53ce29644f15a7c60ce68ede4a3351d56fc45bae
Park hyunjoon <webman7@naver.com>
2023-05-08 10:07:04 +0900

    :art:  비즈니스계정 리스트 회계권한 추가

--- src/main/java/com/adplatform/restApi/domain/business/dto/account/BusinessAccountDto.java
+++ src/main/java/com/adplatform/restApi/domain/business/dto/account/BusinessAccountDto.java
@@ -225,6 +225,7 @@ public class BusinessAccountDto {
             private Integer id;
             private String name;
             private String memberType;
+            private String accountingYn;
             private String memberTypeAccount;
             private Integer adAccountIdMaster;
             private Integer adAccountId;
